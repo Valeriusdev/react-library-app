3845912138d81e1210a811bcd8df1ed1e455939a
Valeriusdev <kakao757@gmail.com>
2024-12-21 18:01:29 -0800

    add Route for NotFoundLayout component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomeLayout from "./layout/HomeLayout";
 import AboutLayout from "./layout/AboutLayout";
+import NotFoundLayout from "./layout/NotFoundLayout";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
           <Route path="/" element={<HomeLayout />} />
           <Route path="/about" element={<AboutLayout />} />
           <Route path="/contacts" element={<ContactsLayout />} />
+          <Route path="*" element={<NotFoundLayout />} />
         </Routes>
       </div>
     </BrowserRouter>
