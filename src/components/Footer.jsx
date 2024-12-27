a32e1d8111dee5be2bd02c4ee247c996cb61047d
Valeriusdev <kakao757@gmail.com>
2024-12-27 03:30:40 -0800

    update Footer component styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="w-screen h-full bg-gray-800 text-white flex items-center justify-center">
+    <footer className="w-full h-full bg-gray-800 text-white flex items-center justify-center">
       <div className="w-full">
         <h2 className="text-5xl text-center">Footer</h2>
       </div>
