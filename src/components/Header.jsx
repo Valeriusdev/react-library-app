52e6614e92e446e1d9ca2582a8c4b3de83fa6224
Valeriusdev <kakao757@gmail.com>
2024-12-27 03:04:52 -0800

    update Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,10 +3,10 @@ import Menu from "./Menu";
 
 const Header = () => {
   return (
-    <header className="h-[40vh] bg-gray-800 text-white flex items-center justify-center">
-      <div className="w-full">
+    <header className="h-[40vh] bg-gray-800 text-white relative">
+      <Menu className="absolute top-4 right-4" />
+      <div className="w-full h-full flex items-center justify-center">
         <h1 className="text-5xl text-center">Header</h1>
-        <Menu />
       </div>
     </header>
   );
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,8 +1,8 @@
 import { Link } from "react-router-dom";
 
-const Menu = () => {
+const Menu = ({ className }) => {
   return (
-    <nav>
+    <nav className={className}>
       <Link to="/">Home</Link>
       <Link to="/about">About</Link>
     </nav>
