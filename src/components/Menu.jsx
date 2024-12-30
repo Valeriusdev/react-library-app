3ae75eeac9f153d80cbc9e2cc90d86b82eeba14f
Valeriusdev <kakao757@gmail.com>
2024-12-29 19:50:15 -0800

    update Menu component styling

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 const Menu = ({ className }) => {
   return (
     <nav className={className}>
-      <Link to="/" className="font-medium text-white hover:text-blue-400 mx-4" >Home</Link>
-      <Link to="/about" className="font-medium text-white hover:text-blue-400 mx-4" >About</Link>
-      <Link to="/contacts" className="font-medium text-white hover:text-blue-400 mx-4">Contacts</Link>
+      <Link to="/" className="font-medium text-white hover:text-blue-400 mx-4 border border-white rounded-full px-4 py-2" >Home</Link>
+      <Link to="/about" className="font-medium text-white hover:text-blue-400 mx-4 border border-white rounded-full px-4 py-2" >About</Link>
+      <Link to="/contacts" className="font-medium text-white hover:text-blue-400 mx-4 border border-white rounded-full px-4 py-2">Contacts</Link>
     </nav>
   );
 };
