d455c534aca4b82119f3dcbcf8c49bb89c07818e
Valeriusdev <kakao757@gmail.com>
2024-12-30 20:06:57 -0800

    add books.js

---
+++ src/data/books.js
@@ -0,0 +1,29 @@
+const books = [
+  {
+    title: "Complete MERN Guide",
+    slug: "mern",
+    id: 1,
+  },
+  {
+    title: "Complete Node.js Guide",
+    slug: "node",
+    id: 2,
+  },
+  {
+    title: "Complete React Guide",
+    slug: "react",
+    id: 3,
+  },
+  {
+    title: "Complete Express.js Guide",
+    slug: "express",
+    id: 4,
+  },
+  {
+    title: "Complete MongoDB Guide",
+    slug: "mongodb",
+    id: 5,
+  },
+];
+
+export default books;
