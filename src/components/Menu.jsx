709f0d4c4360e98f1bbcf742bc59aa56b2cb79b1
Valeriusdev <kakao757@gmail.com>
2024-12-31 04:31:41 -0800

    update Menu component

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -3,10 +3,30 @@ import { Link } from "react-router-dom";
 const Menu = ({ className }) => {
   return (
     <nav className={className}>
-      <Link to="/" className="font-medium text-white hover:text-blue-400 mx-4 border border-white rounded-full px-4 py-2" >Home</Link>
-      <Link to="/" className="font-medium text-white hover:text-blue-400 mx-4 border border-white rounded-full px-4 py-2" >Books</Link>
-      <Link to="/about" className="font-medium text-white hover:text-blue-400 mx-4 border border-white rounded-full px-4 py-2" >About</Link>
-      <Link to="/contacts" className="font-medium text-white hover:text-blue-400 mx-4 border border-white rounded-full px-4 py-2">Contacts</Link>
+      <Link
+        to="/"
+        className="font-medium text-white hover:text-blue-400 mx-4 border border-white rounded-full px-4 py-2"
+      >
+        Home
+      </Link>
+      <Link
+        to="/books"
+        className="font-medium text-white hover:text-blue-400 mx-4 border border-white rounded-full px-4 py-2"
+      >
+        Books
+      </Link>
+      <Link
+        to="/about"
+        className="font-medium text-white hover:text-blue-400 mx-4 border border-white rounded-full px-4 py-2"
+      >
+        About
+      </Link>
+      <Link
+        to="/contacts"
+        className="font-medium text-white hover:text-blue-400 mx-4 border border-white rounded-full px-4 py-2"
+      >
+        Contacts
+      </Link>
     </nav>
   );
 };
