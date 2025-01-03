f0ab5d3c2c44b58142aaf09f510caace26c8ff24
Valeriusdev <kakao757@gmail.com>
2025-01-03 11:30:16 -0800

    update Menu component

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,39 +1,21 @@
 import { NavLink } from "react-router-dom";
 
 const Menu = ({ className }) => {
+  const linkStyle = ({ isActive }) => 
+    `font-medium ${isActive ? 'text-blue-200' : 'text-white'} hover:text-blue-400 mx-4 border border-white rounded-full px-4 py-2`;
+
   return (
     <nav className={className}>
-      <NavLink
-        to="/"
-        end
-        className={({ isActive }) => 
-          `font-medium ${isActive ? 'text-blue-200' : 'text-white'} hover:text-blue-400 mx-4 border border-white rounded-full px-4 py-2`
-        }
-      >
+      <NavLink to="/" end className={linkStyle}>
         Home
       </NavLink>
-      <NavLink
-        to="/books"
-        className={({ isActive }) => 
-          `font-medium ${isActive ? 'text-blue-200' : 'text-white'} hover:text-blue-400 mx-4 border border-white rounded-full px-4 py-2`
-        }
-      >
+      <NavLink to="/books" className={linkStyle}>
         Books
       </NavLink>
-      <NavLink
-        to="/about"
-        className={({ isActive }) => 
-          `font-medium ${isActive ? 'text-blue-200' : 'text-white'} hover:text-blue-400 mx-4 border border-white rounded-full px-4 py-2`
-        }
-      >
+      <NavLink to="/about" className={linkStyle}>
         About
       </NavLink>
-      <NavLink
-        to="/contacts"
-        className={({ isActive }) => 
-          `font-medium ${isActive ? 'text-blue-200' : 'text-white'} hover:text-blue-400 mx-4 border border-white rounded-full px-4 py-2`
-        }
-      >
+      <NavLink to="/contacts" className={linkStyle}>
         Contacts
       </NavLink>
     </nav>
