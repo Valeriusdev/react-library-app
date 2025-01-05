dfc09aab3534b2cf5562c432d84df7ee31e6ebb9
Valeriusdev <kakao757@gmail.com>
2025-01-05 02:38:07 -0800

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomeLayout from "./layout/HomeLayout";
 import BooksLayout from "./layout/BooksLayout";
+import SingleBookLayout from "./layout/SingleBookLayout";
 import AboutLayout from "./layout/AboutLayout";
 import ContactsLayout from "./layout/ContactsLayout";
 import NotFoundLayout from "./layout/NotFoundLayout";
@@ -13,6 +14,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<HomeLayout />} />
           <Route path="/books" element={<BooksLayout />} />
+          <Route path="/books/:slug" element={<SingleBookLayout />} />
           <Route path="/about" element={<AboutLayout />} />
           <Route path="/contacts" element={<ContactsLayout />} />
           <Route path="*" element={<NotFoundLayout />} />
