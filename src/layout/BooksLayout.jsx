981865fd806d8243a36d9ce0a7774cc8952fe390
Valeriusdev <kakao757@gmail.com>
2025-01-06 06:06:24 -0800

    update BooksLayout component

--- src/layout/BooksLayout.jsx
+++ src/layout/BooksLayout.jsx
@@ -1,7 +1,7 @@
-import { Link } from 'react-router-dom'
-import Header from '../components/Header'
-import Footer from '../components/Footer'
-import books from '../data/books'
+import { Link } from "react-router-dom";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import books from "../data/books";
 
 const BooksLayout = () => {
   return (
@@ -15,13 +15,15 @@ const BooksLayout = () => {
             <h1>Books</h1>
           </div>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 max-w-7xl mx-auto">
-            {books.map((book) =>(
-              <Link 
-                to={book.slug} 
+            {books.map((book) => (
+              <Link
+                to={book.slug}
                 key={book.id}
                 className="block p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-200 transition-all"
               >
-                <h2 className="mb-2 text-xl font-bold text-black">{book.title}</h2>
+                <h2 className="mb-2 text-xl font-bold text-black">
+                  {book.title}
+                </h2>
               </Link>
             ))}
           </div>
@@ -31,7 +33,7 @@ const BooksLayout = () => {
         <Footer />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default BooksLayout
+export default BooksLayout;
