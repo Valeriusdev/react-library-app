7fa6c65b9eaa61e0aad546723fcd9249de304da6
Valeriusdev <kakao757@gmail.com>
2025-01-24 19:17:49 -0800

    update Menu component styling

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -3,8 +3,8 @@ import { NavLink } from "react-router-dom";
 const Menu = ({ className }) => {
   const linkStyle = ({ isActive }) =>
     `font-medium ${
-      isActive ? "text-blue-200" : "text-white"
-    } hover:text-blue-400 mx-4 border border-white rounded-full px-4 py-2`;
+      isActive ? "text-blue-400" : "text-white"
+    } hover:text-blue-400 mx-4 border border-white rounded-full px-6 py-2`;
 
   return (
     <nav className={className}>
